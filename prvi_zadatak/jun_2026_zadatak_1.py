bae44248a5a4215c0b0a4eacc35f889fc1cae30c
Aleksa Milošević <aleksa.milosevic807@gmail.com>
2026-07-23 16:50:55 +0200

    Azurirano resenje prvog zadatka za ispitni rok jun 2026 tako da koristi list comprehension sintaksu

--- prvi_zadatak/jun_2026_zadatak_1.py
+++ prvi_zadatak/jun_2026_zadatak_1.py
@@ -9,50 +9,38 @@
 
 # svaki tuple u listi rez -> (tim, grupa, broj bodova, gol razlika)
 
-from itertools import chain
+# List comprehension sintaksa za ugnjezdene petlje:
+# nova_lista = [izraz for x in spoljasnja for y in unutrasnja]
 
-def prolazak(rez:list[tuple[str, int, int, int]], n:int, k:int, m:int, p:int, q:int) -> list[str]:
-    # prvo treba da odredimo p * k timova koji direktno prolaze u narednu fazu
+def prolazak_1(rez:list[tuple[str, int, int, int]], n:int, k:int, m:int, p:int, q:int) -> list[str]:
+    # grupisemo timove u njihove odgovarajuce grupe tako sto kreiramo listu listi
+    # u prvoj podlisti su svi timovi iz grupe 1, u drugoj podlisti su svi timovi iz grupe 2 itd.
+    # u svakoj grupi timovi su sortirani po broju bodova i po gol razlici u opadajucem redosledu
+    timovi_po_grupama = [
+        sorted((tim for tim in rez if tim[1] == grupa + 1), key=lambda x: (x[2], x[3]), reverse=True) 
+        for grupa in range(k)
+    ]
 
-    # pravimo po jednu listu za svaku grupu (ukupno k grupa)
-    timovi_po_grupama = [[] for _ in range(k)]
+    # uzimamo prvih p timova iz svake grupe koji direktno prolaze dalje
+    direktno_prosli_timovi = [tim[0] for grupa in timovi_po_grupama for tim in grupa[:p]]
 
-    # stavljamo timove u odgovarajuce grupe (liste) npr. grupa 1 -> timovi_po_grupama[0], grupa 2 -> timovi_po_grupama[2]
-    for tim in rez:
-        grupa = tim[1]
-        timovi_po_grupama[grupa - 1].append(tim)
+    # od timova sa p+1 mesta iz svake grupe pravimo novu listu (grupu) tako sto ih sortiramo
+    # po broju bodova i gol razlici u opadajucem redosledu
+    # ako postoje timovi sa p+2, p+3, itd. mesta u grupama oni se ignorisu jer sigurno ne prolaze dalje
+    timovi_kandidati_za_prolazak = sorted(
+        (grupa[p] for grupa in timovi_po_grupama if len(grupa) > p),
+        key=lambda tim: (tim[2], tim[3]), reverse=True
+    )
 
-    timovi_koji_su_prosli = []
-    # u svakoj grupi sortiramo timove po broju bodova, pa onda po gol razlici u opadajucem redosledu
-    # tako da je prvi tim u grupi onaj sa najvise bodova
-    # (ili sa najboljom gol razlikom ako u grupi ima dva tima sa istim brojem bodova)
-    # onda brisemo timove od p+1 mesta pa do kraja grupe jer oni sigurno ne prolaze
-    # npr. ako prolaze prva k=3 tima iz svake grupe onda od ostalih timova koji nisu zauzeli jedno od
-    # prva tri mesta u svojoj grupi ulaze u razmatranje za prolaz samo timovi koji su zauzeli k+1=4 mesto
-    # u svojoj grupi
-    # u ugnjezdenoj petlji uzimamo samo timove koji direktno prolaze npr. ako je k=3 onda prva 3 tima iz svake grupe
-    for grupa in timovi_po_grupama:
-        grupa.sort(key=lambda tim: (tim[2], tim[3]), reverse=True)
-        del grupa[p+1:]
-        for _ in range(p):
-            tim_koji_direktno_prolazi = grupa.pop(0)
-            timovi_koji_su_prosli.append(tim_koji_direktno_prolazi[0])
+    # od timova sa p+1 mesta iz svih grupa dodatna prolaze q najboljih po bodovima i gol razlici
+    dodatno_prosli_timovi = [tim[0] for tim in timovi_kandidati_za_prolazak[:q]]
 
-    # spajamo sve preostale timove u jednu listu (grupu)
-    preostali_timovi = list(chain.from_iterable(timovi_po_grupama))
-    # sortiramo ih tako da je prvi tim u listi (grupi) onaj sa najvise bodova
-    # (ili sa najboljom gol razlikom ako ima vise timova sa istim brojem bodova)
-    preostali_timovi.sort(key=lambda tim: (tim[2], tim[3]), reverse=True)
-    # od preostalih timova uzimamo jos q najboljih za prolaz
-    for _ in range(q):
-        tim_koji_dodatno_prolazi = preostali_timovi.pop(0)
-        timovi_koji_su_prosli.append(tim_koji_dodatno_prolazi[0])
-
-    return timovi_koji_su_prosli
+    prosli_timovi = direktno_prosli_timovi + dodatno_prosli_timovi
+    return prosli_timovi
 
 if __name__ == "__main__":
-    timovi_koji_su_prosli = prolazak(rez=[("T1", 1, 3, 1), ("T2", 3, 7, 4), ("T3", 2, 6, 1), ("T4", 2, 3, 0),
-            ("T5", 3, 5, 2), ("T6", 1, 4, 1), ("T7", 1, 4, 2), ("T8", 1, 6, 3),
-            ("T9", 2, 9, 7), ("T10", 3, 1, -2), ("T11", 3, 3, -1), ("T12", 2, 0, -4)],
-            n=12, k=3, m=8, p=3, q=2)
-    print(timovi_koji_su_prosli)
+    prosli_timovi = prolazak_1(rez=[("T1", 1, 3, 1), ("T2", 3, 7, 4), ("T3", 2, 6, 1), ("T4", 2, 3, 0),
+             ("T5", 3, 5, 2), ("T6", 1, 4, 1), ("T7", 1, 4, 2), ("T8", 1, 6, 3),
+             ("T9", 2, 9, 7), ("T10", 3, 1, -2), ("T11", 3, 3, -1), ("T12", 2, 0, -4)],
+             n=12, k=3, m=8, p=3, q=2)
+    print(prosli_timovi)
